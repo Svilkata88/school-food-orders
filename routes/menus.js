a9cce9ba2140a34d3016b7fb96ded06490c3a920
Svilen Ivanov <134698218+Svilkata88@users.noreply.github.com>
2025-10-29 23:08:37 +0200

    add: 404 page, fix: fixe templates and menus.js

--- routes/menus.js
+++ routes/menus.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const isUser = require('../middleware/isUser');
 const {getMenus, saveMenu} = require('../services/menusServices');
-const {getMenus} = require('../services/menusServices');
 const {getUserFromToken} = require('../utils');
 const menusRouter = express.Router();
 
@@ -14,18 +13,18 @@ menusRouter.get('/', isUser, (req, res) => {
     res.render('menus', { title: 'Menus', message: 'All menus listed.', menus, currentUser });
 })
 
-menusRouter.get('/:name', isUser, (req, res) => {
-    const { name } = req.params;
+menusRouter.get('/create-menu', isUser, (req, res) => {
+    res.render('create-menu', { title: 'Create Menu', message: 'Create a menu.' })
+})
+
+menusRouter.get('/:menuTitle', isUser, (req, res) => {
+    const { menuTitle } = req.params;
     const menus = getMenus();
-    const menu = menus.find(m => m.name === name);
+    const menu = menus.find(m => m.menuTitle === menuTitle);
     if (!menu) {
         return res.status(404).render('404', { title: 'Menu Not Found', message: 'The requested menu was not found.' });
     }
-    res.render('menu', { title: menu.name, message: menu.description, menu });
-})
-
-menusRouter.get('/create-menu', (req, res) => {
-    res.render('create-menu', { title: 'Create Menu', message: 'Create a menu.' })
+    res.render('menu', { title: menu.menuTitle, message: menu.days, menu });
 })
 
 menusRouter.post('/create-menu', (req, res) => {
